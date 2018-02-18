fd97a9bcb47587c2f8452f362574115d12c5cbb8
Dan Lindsay <dlindsay72@hotmail.com>
2018-02-18 12:37:27 -0500

    startRecording method done

--- ScreenCatch/ViewController.swift
+++ ScreenCatch/ViewController.swift
@@ -19,14 +19,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var selectedImage: UIImageView!
     
     var recorder = RPScreenRecorder.shared()
+    private var isRecording = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    @IBAction func recordBtnWasPressed(_ sender: Any) {
+    func startRecording() {
+        guard recorder.isAvailable else {
+            print("Recording is not available at this time")
+            return
+        }
         
+        if micSwitch.isOn {
+            recorder.isMicrophoneEnabled = true
+        } else {
+            recorder.isMicrophoneEnabled = false
+        }
+        
+        recorder.startRecording { (error) in
+            guard error == nil else {
+                print("There was an error starting the recording")
+                return
+            }
+            print("STARTED RECORING")
+            
+            DispatchQueue.main.async {
+                self.micSwitch.isEnabled = false
+                self.recordBtn.setTitleColor(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1), for: .normal)
+                self.recordBtn.setTitle("Stop", for: .normal)
+                self.statusLbl.text = "Recording..."
+                self.statusLbl.textColor = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)
+                self.isRecording = true
+            }
+            
+        }
+    }
+    
+    func stopRecording() {
+        
+    }
+    
+    @IBAction func recordBtnWasPressed(_ sender: Any) {
+        if !isRecording {
+            startRecording()
+        } else {
+            stopRecording()
+        }
     }
     
     @IBAction func imagePicked(_ sender: UISegmentedControl) {
